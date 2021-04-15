8e9d136c3b0ab71a5b477b40862bbd3cd6a5efd0
reddyyuvakishore1211 <79675559+reddyyuvakishore1211@users.noreply.github.com>
2021-04-15 18:09:12 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from app.models import *
 from django.db.models.functions import Length
+from django.db.models import Q
 
 def display_topic(request):
     #topics=Topic.objects.all()
@@ -17,14 +18,28 @@ def display_webpage(request):
     #webpages=Webpage.objects.all().order_by('name')
     #webpages=Webpage.objects.all().order_by('-name')
     #webpages=Webpage.objects.all().order_by(Length('name'))
-    webpages=Webpage.objects.all().order_by(Length('name').desc())
-    webpages=Webpage.objects.filter(url__startswith='http:')
-    webpages=Webpage.objects.filter(url__endswith='.com')
-    webpages=Webpage.objects.filter(name__icontains='h')
+    #webpages=Webpage.objects.all().order_by(Length('name').desc())
+    #webpages=Webpage.objects.filter(url__startswith='http:')
+    #webpages=Webpage.objects.filter(url__endswith='.com')
+    #webpages=Webpage.objects.filter(name__icontains='h')
+    webpages=Webpage.objects.filter(Q(topic_name='Tennis') & Q(name='Michael'))
+    #webpages=Webpage.objects.filter(Q(topic_name='Cricket') | Q(name='Walter') | Q(url='https://manning.info/'))
+    webpages=Webpage.objects.filter(name__in=['Dhoni','Patricia'])
+    webpages=Webpage.objects.filter(name__regex=r'^[a-zA-Z]{1}h')
     d={'webpages':webpages}
+
     return render(request,'display_webpage.html',context=d)
 
 def display_access(request):
     access=Access_Records.objects.all()
+    #access=Access_Records.objects.filter(date__gt='1995-11-11')
+    #access=Access_Records.objects.filter(date__year='2003')
+    #access=Access_Records.objects.filter(date__month='05')
+    access=Access_Records.objects.filter(date__day='17')
+    
+    
     d={'access':access}
     return render(request,'display_access.html',d)
+
+
+
